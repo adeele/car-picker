5885e65eb29c66c0cd5c85db4ce1ea207eae912f
Adrianna Małkiewicz-Błotniak <adrianna.malkiewicz@gmail.com>
2020-11-01 13:50:20 +0100

    Refactor state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Picker from "./Picker";
-import withFetch from "./withFetch";
 import VehicleBrowser from "./VehicleBrowser";
 import ErrorMessage from "./ErrorMessage";
 
-const PickerWithFetch = withFetch(Picker);
-const VehicleBrowserWithFetch = withFetch(VehicleBrowser);
-
 const App = () => {
-    const [make, setMake] = useState('');
-    const [model, setModel] = useState('');
     const [error, setError] = useState(null);
 
-    return <React.Fragment>
+    const [{ makes, make, models, model, vehicles }, setState] = useState({});
+
+    const doFetch = (type, params, callback) => {
+        fetch(`http://localhost:8080/api/${type}s?${new URLSearchParams(params)}`)
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    callback(JSON.parse(localStorage.getItem(type)));
+                    throw new Error("An error occurred. Application may not work properly.");
+                }
+            })
+            .then((response) => {
+                localStorage.setItem(type, JSON.stringify(response));
+                callback(response);
+            })
+            .catch(setError);
+    }
+
+    useEffect(() => {
+        doFetch('make', {}, (makes) => setState({ makes }));
+    }, []);
+
+    const onMakePick = (make) => {
+        setState(({ makes }) => ({ makes }));
+        if (make) {
+            doFetch(
+                'model',
+                {make},
+                (models) => setState((prevState) => ({ ...prevState, make, models })));
+        }
+    }
+
+    const onModelPick = (model) => {
+        setState(({ models, makes, make }) => ({ models, makes, make }));
+        if (model) {
+            doFetch(
+                'vehicle',
+                { make, model },
+                (vehicles) => setState((prevState => ({ ...prevState, model, vehicles }))));
+        }
+    }
+
+    return <>
         <header>Car picker</header>
         <form>
-            <PickerWithFetch type="make" onPick={setMake} error={error} errorHandler={setError} />
-            <PickerWithFetch type="model"
-                             disabled={!make}
-                             params={{ 'make': make }}
-                             onPick={setModel}
-                             error={error}
-                             errorHandler={setError} />
-            <VehicleBrowserWithFetch type="vehicle"
-                                     disabled={!model}
-                                     params={{ 'make': make, 'model': model }}
-                                     error={error}
-                                     errorHandler={setError} />
+            <Picker type="make" onPick={onMakePick} elements={makes} />
+            <Picker type="model" onPick={onModelPick} elements={models} />
+            <VehicleBrowser type="vehicle" disabled={!model} elements={vehicles} />
         </form>
         <ErrorMessage error={error} />
-    </React.Fragment>;
+    </>;
 };
 
 export default App;
--- src/Picker.jsx
+++ src/Picker.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Picker = ({ type, elements, disabled, onPick }) => {
-    const setElement = ({ target: { value }}) => {
-        const element = elements.includes(value) ? value : '';
+const Picker = ({ type, elements, onPick }) => {
+    const [value, setValue] = useState('');
+
+    const handleChange = ({ target: { value }}) => {
+        setValue(value);
+
+        const element = elements && elements.includes(value) ? value : '';
 
         onPick(element);
     }
 
-    return <div className={`picker ${disabled && "disabled"}`}>
+    if (!elements && value) {
+        setValue('');
+    }
+
+    return <div className={`picker ${elements && "disabled"}`}>
         <label>{`Choose ${type}:`}</label>
-        <input type="text" list={type} onChange={setElement} />
+        <input type="text" list={type} onChange={handleChange} value={value}/>
         <datalist id={type}>
             {elements && elements.map((element, index) => <option value={element} key={index} />)}
         </datalist>
